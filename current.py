179bc6184aceb4226b51a8edc4a5ec4197e09864
kimito_k <kimito_k@MacBook-Pro.local>
2020-11-21 06:25:35 +0900

    step7

--- current.py
+++ current.py
@@ -4,13 +4,28 @@ class Variable:
     def __init__(self, data):
         self.data = data
         self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        f = self.creator
+        if f is not None:
+            x = f.input
+            x.grad = f.backward(self.grad)
+            x.backward()
 
 class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
+
         output = Variable(y)
+        output.set_creator(self)
+
         self.input = input
+        self.output = output
         return output
 
     def forward(self, x):
@@ -52,9 +67,10 @@ if __name__ == '__main__':
     C = Square()
 
     x = Variable(np.array(0.5))
-    y = C(B(A(x)))
-    print(y.data)
+    a = A(x)
+    b = B(a)
+    y = C(b)
 
     y.grad = np.array(1.0)
-    x.grad = C.backward(B.backward(A.backward(y.grad)))
+    y.backward() 
     print(x.grad)
