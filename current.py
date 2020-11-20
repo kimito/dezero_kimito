fcbc2e1a3e43f38bf0b9d4f4ee7024a7ee7233b0
kimito_k <kimito_k@MacBook-Pro.local>
2020-11-21 06:50:22 +0900

    step9

--- current.py
+++ current.py
@@ -1,7 +1,16 @@
 import numpy as np
 
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
 class Variable:
     def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{} is not supported'.format(type(data)))
+
         self.data = data
         self.grad = None
         self.creator = None
@@ -10,6 +19,9 @@ class Variable:
         self.creator = func
 
     def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
         funcs = [self.creator]
         while funcs:
             f = funcs.pop()
@@ -24,7 +36,7 @@ class Function:
         x = input.data
         y = self.forward(x)
 
-        output = Variable(y)
+        output = Variable(as_array(y))
         output.set_creator(self)
 
         self.input = input
@@ -47,6 +59,10 @@ class Square(Function):
         gx = 2 * x * gy
         return gx
 
+def square(x):
+    return Square()(x)
+
+
 class Exp(Function):
     def forward(self, x):
         y = np.exp(x)
@@ -57,23 +73,12 @@ class Exp(Function):
         gx = np.exp(x) * gy
         return gx
 
-def numerical_diff(f, x, eps=1e-4):
-    x0 = Variable(x.data - eps)
-    x1 = Variable(x.data + eps)
-    y0 = f(x0)
-    y1 = f(x1)
-    return (y1.data - y0.data)/(2 * eps)
+def exp(x):
+    return Exp()(x)
 
 if __name__ == '__main__':
-    A = Square()
-    B = Exp()
-    C = Square()
-
     x = Variable(np.array(0.5))
-    a = A(x)
-    b = B(a)
-    y = C(b)
+    y = square(exp(square(x)))
 
-    y.grad = np.array(1.0)
     y.backward() 
     print(x.grad)
