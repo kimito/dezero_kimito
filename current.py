18d07470d315d9c2a1b3ab6ae99d50ee0d3dc2c9
kimito_k <kimito_k@MacBook-Pro.local>
2021-01-16 07:36:11 +0900

    step28

--- current.py
+++ current.py
@@ -57,6 +57,9 @@ def my_sin(x, threshold=0.0001):
             break
     return y
 
+def rosenbrock(x0, x1):
+    y = 100 * (x1 - x0 ** 2) ** 2 + (x0 - 1) ** 2
+    return y
 
 def numerical_diff(f, x, eps=1e-4):
     x0 = Variable(x.data - eps)
@@ -91,10 +94,21 @@ def numerical_diff(f, x, eps=1e-4):
 if __name__ == '__main__':
     # unittest.main()
 
-    x = Variable(np.array(np.pi/4))
-    # x = Variable(np.array(0))
-    y = my_sin(x)
-    y.backward()
+    x0 = Variable(np.array(0.0))
+    x1 = Variable(np.array(2.0))
 
-    print(y.data)
-    print(x.grad)
+    lr = 0.001
+    iters = 10000
+
+    for i in range(iters):
+        print(x0, x1)
+
+        y = rosenbrock(x0, x1)
+
+        x0.cleargrad()
+        x1.cleargrad()
+
+        y.backward()
+
+        x0.data -= lr * x0.grad
+        x1.data -= lr * x1.grad
