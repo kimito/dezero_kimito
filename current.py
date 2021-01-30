19352a4d479cb24f57f0aeccbe0eb72c5c6e1cc6
Kimito Kasahara <1219kk@gmail.com>
2021-01-30 16:18:29 +0900

    step34

--- current.py
+++ current.py
@@ -3,115 +3,22 @@ if '__file__' in globals():
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import numpy as np
-import math
-import unittest
-from dezero import Variable, Function
+from dezero import Variable
+import dezero.functions as F
+import dezero.utils as P
 
-# class Square(Function):
-#     def forward(self, x):
-#         y = np.power(x, 2)
-#         return y
+x = Variable(np.array(1.0))
+x.name = 'x'
+y = F.sin(x)
+y.name = 'y'
+y.backward(create_graph=True)
 
-#     def backward(self, gy):
-#         x = self.inputs[0].data
-#         gx = 2 * x * gy
-#         return gx
+for i in range(3):
+    gx = x.grad
+    x.grad.name = "gx" + str(i)
+    x.cleargrad()
+    gx.backward(create_graph=True)
+    print(x.grad)
 
-# def square(x):
-#     return Square()(x)
-
-
-# class Exp(Function):
-#     def forward(self, x):
-#         y = np.exp(x)
-#         return y
-    
-#     def backward(self, gy):
-#         x = self.inputs[0].data
-#         gx = np.exp(x) * gy
-#         return gx
-
-# def exp(x):
-#     return Exp()(x)
-
-class Sin(Function):
-    def forward(self, x):
-        y = np.sin(x)
-        return y
-
-    def backward(self, gy):
-        x = self.inputs[0].data
-        gx = gy * np.cos(x)
-        return gx
-
-def sin(x):
-    return Sin()(x)
-
-def my_sin(x, threshold=0.0001):
-    y = 0
-    for i in range(100000):
-        c = (-1) ** i / math.factorial(2 * i + 1)
-        t = c * x ** (2 * i + 1)
-        y = y + t
-        if abs(t.data) < threshold:
-            break
-    return y
-
-def rosenbrock(x0, x1):
-    y = 100 * (x1 - x0 ** 2) ** 2 + (x0 - 1) ** 2
-    return y
-
-def numerical_diff(f, x, eps=1e-4):
-    x0 = Variable(x.data - eps)
-    x1 = Variable(x.data + eps)
-    y0 = f(x0)
-    y1 = f(x1)
-    return (y1.data - y0.data)/(2 * eps)
-
-# class SquareTest(unittest.TestCase):
-#     def test_forward(self):
-#         x = Variable(np.array(2.0))
-#         y = square(x)
-#         expected = np.array(4.0)
-#         self.assertEqual(y.data, expected)
-
-#     def test_backward(self):
-#         x = Variable(np.array(3.0))
-#         y = square(x)
-#         y.backward()
-#         expected = np.array(6.0)
-#         self.assertEqual(x.grad, expected)
-
-#     def test_gradient_check(self):
-#         x = Variable(np.random.rand(1))
-#         y = square(x)
-#         y.backward()
-#         num_grad = numerical_diff(square, x)
-#         flg = np.allclose(x.grad, num_grad)
-#         self.assertTrue(flg)
-
-
-if __name__ == '__main__':
-    # unittest.main()
-
-    def f(x):
-        y = x ** 4 - 2 * x ** 2
-        return y
-
-    x = Variable(np.array(2.0))
-
-    iters = 10
-
-    for i in range(iters):
-        print(i, x)
-
-        y = f(x)
-        x.cleargrad()
-        y.backward(create_graph=True)
-
-        gx = x.grad
-        x.cleargrad()
-        gx.backward()
-        gx2 = x.grad
-        
-        x.data -= gx.data / gx2.data
+    if i == 2:
+        P.plot_dot_graph(x.grad)
--- dezero/utils.py
+++ dezero/utils.py
@@ -25,7 +25,7 @@ def _dot_var(v, verbose=False):
     if verbose and v.data is not None:
         if v.name is not None:
             name += ": "
-        name += str(v.shape) + ' ' + str(v.dtype)
+        name += str(v.shape) + ' ' + str(v.dtype) + ';' + str(v.data)
     return dot_var.format(id(v), name)
 
 def _dot_func(f):
