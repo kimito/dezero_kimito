cdaa147972c91ce5918fcab42914a8ddb718ca7a
Kimito Kasahara <1219kk@gmail.com>
2021-02-06 09:59:24 +0900

    step41

--- current.py
+++ current.py
@@ -7,10 +7,10 @@ from dezero import Variable
 import dezero.functions as F
 import dezero.utils as P
 
-x0 = Variable(np.array([1, 2, 3]))
-x1 = Variable(np.array([10]))
-y = x0 + x1
-print(y)
-
+x = Variable(np.random.randn(2, 3))
+W = Variable(np.random.randn(3, 4))
+y = F.matmul(x, W)
 y.backward()
-print(x1.grad)
+
+print(x.grad.shape)
+print(W.grad.shape)
